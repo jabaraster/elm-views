598bc69afcf1bbb591bb519fd92537f19c8f0bd4
jabaraster <ah@jabara.info>
2019-04-16 23:40:10 +0900

    Add function [fasCheck]

--- src/Jabara/Views.elm
+++ src/Jabara/Views.elm
@@ -1,6 +1,7 @@
 module Jabara.Views exposing
     ( fas
     , fas_
+    , fasCheck
     )
 
 {-| This module provides some views.
@@ -8,12 +9,13 @@ module Jabara.Views exposing
 
 # Font Awesome Icon.
 
-@docs fas, fas_
+@docs fas, fas_, fasCheck
 
 -}
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 {-| Font Awesome Icon with class attributes.
@@ -36,3 +38,40 @@ Please refer Font Awesome css from html file.
 fas_ : String -> Html msg
 fas_ iconName =
     i [ class <| "fas fa-" ++ iconName ] []
+
+
+{-| A little cool checkbox by Font Awesome Icon.
+Please refer Font Awesome css from html file.
+
+    View only:
+        fasCheck True "Alwaiy Checked" Nothing
+
+    With action:
+        fasCheck True "Alwaiy Checked" (Just SomeMessage)
+-}
+fasCheck : Bool -> String -> Maybe msg -> Html msg
+fasCheck checked label mAction =
+            span
+                ([ classList [("fas-check", True), ("clickable", isJust mAction)] ]
+                    ++ (Maybe.withDefault [] <| Maybe.map (List.singleton  << onClick) mAction))
+                [ fas
+                    (if checked then
+                        "true"
+
+                     else
+                        "false"
+                    )
+                    (if checked then
+                        "check-circle"
+
+                     else
+                        "times-circle"
+                    )
+                , span [] [ text label ]
+                ]
+
+isJust : Maybe a -> Bool
+isJust m =
+    case m of
+        Just _  -> True
+        Nothing -> False
